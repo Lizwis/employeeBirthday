0ec55defef87c925fb66399df97d5e5a8fa5b80c
Lizwi Silimela <lsilimela23@gmail.com>
2022-06-10 08:12:41 -0700

    check leap year

--- app/Http/Controllers/employeeController.php
+++ app/Http/Controllers/employeeController.php
@@ -4,24 +4,34 @@ namespace App\Http\Controllers;
  
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Http;
+use DateTime;
 
 
 class employeeController extends Controller
 {
+
+
+    public function __construct()
+    {
+        $this->todaysDate =  new DateTime();
+    }
   
+
     public function index()
     {
-       $employees = Http::get('https://interview-assessment-1.realmdigital.co.za/employees')->json();
-
-       $todaysDate = date("m-d");
-
+      $employees = Http::get('https://interview-assessment-1.realmdigital.co.za/employees')->json();
+      
+      
         foreach($employees as $employee){
 
+            
             $dateOfBirthConvert = date_create($employee['dateOfBirth']);
             
             $dateOfBirth = $dateOfBirthConvert->format("m-d");
 
-            if($dateOfBirth == $todaysDate){
+            $checkDOB = $this->leapYear($dateOfBirth, $dateOfBirthConvert);
+            
+            if($checkDOB == $this->todaysDate->format('m-d')){
                 $this->sendEmail();
             }
 
@@ -31,5 +41,18 @@ class employeeController extends Controller
 
     private function sendEmail(){
         //send email here
+    }
+
+    private function leapYear($dateOfBirth, $dateOfBirthConvert){
+
+        if($this->todaysDate->format('L') == '1'){
+            if($this->todaysDate->format('m') == '02' && $dateOfBirthConvert->format('d')=='29' ){
+           
+             $dateOfBirth = '02-28';
+           }
+         }
+
+         return $dateOfBirth;
+
     }
 }
